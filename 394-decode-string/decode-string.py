8b1c14a12edf72c263943dc42b126c2af8017b0a
Anuj Kushwaha <anujkush2431393@gmail.com>
2026-03-09 18:32:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.45%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        num_stk, str_stk = [], []
+        num, res = 0, ''
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+            elif c == '[':
+                num_stk.append(num)
+                str_stk.append(res) # initial, res being pushed is empty str ''
+                num, res = 0, ''
+            elif c == ']':
+                res = str_stk.pop() + res * num_stk.pop()
+            else:
+                res += c
+        return res
